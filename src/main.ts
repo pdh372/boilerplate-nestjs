b5619fb879c77e7deafee56cb640bc1928ce41a1
Yuh Mahp <pdh372@gmail.com>
2023-12-12 00:17:42 +0700

    fea: test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
-import { ENV_VAR } from './configs/env.config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,9 +9,5 @@ async function bootstrap() {
         type: VersioningType.URI,
     });
     await app.listen(3000);
-
-    const configService = app.get(ConfigService);
-    const postgresqlUrl = configService.get<string>(ENV_VAR.POSTGRESQL_URL);
-    console.log(postgresqlUrl);
 }
 bootstrap();
